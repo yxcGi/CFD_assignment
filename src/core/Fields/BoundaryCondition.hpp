825ace052782cb9cb4e5ca57b0bac2081938c0cf
yxc <17779028786@163.com>
2025-11-12 11:07:21 +0800

    Added a gradient member variable for the previous time step in the Field class and calculated the value when calling the setValue() function.

--- src/core/Fields/BoundaryCondition.hpp
+++ src/core/Fields/BoundaryCondition.hpp
@@ -1,17 +1,20 @@
 #pragma once
 
 #include <string>
-
-
+#include "Mesh.h"
+#include "BoundaryPatch.h"
 // a * φ + b * ∂φ/∂n = c
 
 template<typename Tp>
 class BoundaryCondition
 {
+    using BType = BoundaryPatch::BoundaryType;
     using ULL = unsigned long long;
     using Scalar = double;
 public:
     BoundaryCondition() = delete;
+    // BoundaryCondition(Mesh* mesh); //
+    BoundaryCondition(const BoundaryPatch& patch);
     BoundaryCondition(const std::string& name, Scalar a, Scalar b, const Tp& c);
     BoundaryCondition(const BoundaryCondition<Tp>&) = delete;
     BoundaryCondition(BoundaryCondition&&) = delete;
@@ -26,20 +29,43 @@ public:
     Scalar get_a() const;
     Scalar get_b() const;
     const Tp& get_c() const;
+    void setValid(bool flag = true);
+
+    // 已经设置
+    bool isValid() const;
 
 private:
     std::string name_;      // 边界名
     Scalar a_;
-    Scalar b_;      
+    Scalar b_;
     Tp c_;                   // 标量/向量/张量
 
     ULL nFace_;         // 边界面数量   
     ULL startFace_;     // 边界起始面编号
-
+    BType type_;
+    bool isSet_ = false;
 };
 
 #pragma region 函数实现
 
+// template<typename Tp>
+// inline BoundaryCondition<Tp>::BoundaryCondition(Mesh* mesh)
+// {
+//     using BType = BoundaryPatch::BoundaryType;
+//     const std::unordered_map<std::string, BoundaryPatch>& boundaryPatches = mesh->getBoundaryPatches();
+//     for (const auto& [name, patch] : boundaryPatches)
+//     {
+        
+//     }
+// }
+
+template<typename Tp>
+inline BoundaryCondition<Tp>::BoundaryCondition(const BoundaryPatch& patch)
+    : name_(patch.getName())
+    , nFace_(patch.getNFace())
+    , startFace_(patch.getStartFace())
+    , type_(patch.getType())
+{}
 template<typename Tp>
 inline BoundaryCondition<Tp>::BoundaryCondition(const std::string& name, Scalar a, Scalar b, const Tp& c)
     : name_(name)
@@ -82,5 +108,17 @@ template<typename Tp>
 inline const Tp& BoundaryCondition<Tp>::get_c() const
 {
     return c_;
+}
+
+template<typename Tp>
+inline void BoundaryCondition<Tp>::setValid(bool flag)
+{
+    isSet_ = flag;
+}
+
+template<typename Tp>
+inline bool BoundaryCondition<Tp>::isValid() const
+{
+    return isSet_;
 }
 
